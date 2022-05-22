e96556830bab9f758d91db80a8584c9454554bdc
James Robertson <digital.robertson@gmail.com>
2022-05-22 13:37:48 +0100

    feature: The user can now play one of their Amazon ebooks through the audio player.

--- lib/alexa_remotectl.rb
+++ lib/alexa_remotectl.rb
@@ -58,14 +58,20 @@ end
 
 class AlexaRemoteCtl
 
-  def initialize(domain: 'alexa.amazon.co.uk', device: {}, cookie: '')
+  def initialize(domain: 'alexa.amazon.co.uk', device: {}, cookie: '',
+                 customerid: '')
 
-    @domain, @device, @cookie = domain, device, cookie
+    @domain, @device, @cookie, @customerid = domain, device, cookie, customerid
 
   end
 
   def info()
     device_player()[:playerInfo]
+  end
+
+  def list_ebooks()
+    get_json '/api/ebooks/library', "mediaOwnerCustomerId=#{@customerid}" +
+        "&nextToken=&size=50"
   end
 
   def mute?()
@@ -85,6 +91,28 @@ class AlexaRemoteCtl
 
   def play
     pp_cmd('Play')
+  end
+
+  def play_ebook(id)
+
+    serialno = @device[:serialno]
+    type = @device[:type]
+
+    url = "https://#{@domain}/api/ebooks/queue-and-play?"
+
+    body = {
+      'asin' => id,
+      'deviceType' => type,
+      'deviceSerialNumber' => serialno,
+      'mediaOwnerCustomerId' => @customerid
+    }.to_json
+
+    post_request url, body
+
+  end
+
+  def play_hq(id)
+    device_phq(id)
   end
 
   def playing?()
@@ -136,18 +164,51 @@ class AlexaRemoteCtl
     serialno = @device[:serialno]
     type = @device[:type]
 
-    url = "https://#{@domain}/api/np/command?deviceSerialNumber=#{serialno}&deviceType=#{type}"
+    url = "https://#{@domain}/api/np/command?deviceSerial" +
+        "Number=#{serialno}&deviceType=#{type}"
     post_command url, body
 
   end
 
-  def device_player()
+  # play historical queue (PHQ)
+  #
+  def device_phq(queueid)
+
     serialno = @device[:serialno]
     type = @device[:type]
 
-    url = "https://#{@domain}/api/np/player?deviceSerialNumber=#{serialno}&deviceType=#{type}"
-    r = post_request url
+    url = "https://#{@domain}/api/media/play-historical-queue"
+
+    body = {
+      'deviceType' => type,
+      'deviceSerialNumber' => serialno,
+      'queueId' => queueid,
+      'startTime' => nil,
+      'service' => nil,
+      'trackSource' => nil,
+      'mediaOwnerCustomerId' => @customerid
+    }.to_json
+
+    post_request url, body
+
+  end
+
+  def device_player()
+    get_json '/api/np/player'
+  end
+
+  def get_json(uri, params='')
+
+    serialno = @device[:serialno]
+    type = @device[:type]
+
+    url = "https://#{@domain}#{uri}?deviceSerial" +
+        "Number=#{serialno}&deviceType=#{type}"
+    url += '&' + params if params.length > 1
+
+    r = get_request url
     JSON.parse(r.body, symbolize_names: true)
+
   end
 
   def make_response(uri, request)
@@ -155,14 +216,15 @@ class AlexaRemoteCtl
     request["Accept"] = "application/json, text/javascript, */*; q=0.01"
     request["Accept-Language"] = "en-GB,en-US;q=0.9,en;q=0.8"
     request["Connection"] = "keep-alive"
-    request["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8"
     request["Cookie"] = @cookie
 
     request["Referer"] = "https://#{@domain}/spa/index.html"
     request["Sec-Fetch-Dest"] = "empty"
     request["Sec-Fetch-Mode"] = "cors"
     request["Sec-Fetch-Site"] = "same-origin"
-    request["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36"
+    request["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64) " +
+        "AppleWebKit/537.36 (KHTML, like Gecko) " +
+        "Chrome/101.0.4951.64 Safari/537.36"
     request["X-Requested-With"] = "XMLHttpRequest"
     request["csrf"] = "-990908140"
     request["dnt"] = "1"
@@ -181,6 +243,7 @@ class AlexaRemoteCtl
   end
 
   def post_command(url, body='')
+
     uri = URI.parse(url)
     request = Net::HTTP::Post.new(uri)
     request.body = body
@@ -193,10 +256,23 @@ class AlexaRemoteCtl
     # response.body
   end
 
-  def post_request(url)
+  def get_request(url)
 
     uri = URI.parse(url)
     request = Net::HTTP::Get.new(uri)
+    response = make_response(uri, request)
+
+    # response.code
+    # response.body
+
+  end
+
+
+  def post_request(url, body='')
+
+    uri = URI.parse(url)
+    request = Net::HTTP::Post.new(uri)
+    request.body = body
     response = make_response(uri, request)
 
     # response.code
@@ -212,16 +288,17 @@ class AlexaDevices
   # note: label can be any identifier you choose e.g. kitchen
   #
   def initialize(devicesx=[], devices: devicesx,
-                 domain: 'alexa.amazon.co.uk', cookie: '')
+                 domain: 'alexa.amazon.co.uk', cookie: '', customerid: '')
 
     @devices, @domain, @cookie = devices, domain, cookie
+    @customerid = customerid
 
   end
 
   def playing()
 
     @devices.map do |device, label|
-      alexa = AlexaRemoteCtl.new(cookie: @cookie, device: device)
+      alexa = get_alexa device
       alexa.playing? ? [alexa, label] : nil
     end.compact
 
@@ -253,18 +330,28 @@ class AlexaDevices
     if id then
 
       device, _ = a.find {|_, label| label.to_sym == id.to_sym}
-      alexa = AlexaRemoteCtl.new(cookie: @cookie, device: device)
+      alexa = get_alexa device
       alexa.play
 
     else
 
       a.each do |device, label|
+
         puts 'Pausing @' + label.inspect
-        alexa = AlexaRemoteCtl.new(cookie: @cookie, device: device)
+        alexa = get_alexa device
         alexa.play
+
       end
 
     end
+  end
+
+  private
+
+  def get_alexa(device)
+
+    AlexaRemoteCtl.new(cookie: @cookie, device: device,
+                       customerid: @customerid)
   end
 
 end
